efb4eea5728d5e3931bf3172e37d835e4483fd66
Masahiro Honda <honda@mechatrax.com>
2022-02-01 14:41:39 +0900

    Modify start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ $(sleepi3ctl get wakeup-status) -eq 1 ] && sleepi3alarm clear
+
 sleepi3ctl set extin-trigger 0
 sleepi3ctl set ri-trigger 0
 sleepi3ctl set extin-powerdown $EXTIN_FORCED_SHUTDOWN
@@ -9,3 +11,4 @@ sleepi3ctl set restart 1
 sleepi3ctl set measurement-interval 1
 sleepi3ctl set restore-voltage 0
 sleepi3ctl set uvlo 0
+
